bde0c6c5e669c3190b82399560df6bbadb0182dd
Felix Zapata <felix.zapata@adesis.com>
2014-07-06 12:29:32 +0200

    style()

--- europac/wp-content/themes/europac/js/lugares.js
+++ europac/wp-content/themes/europac/js/lugares.js
@@ -1,145 +1,102 @@
 {
-    "paises": [
-        {
-            "pais": "España",
-            "lugares": [
-                {
-                    "lugar": "Madrid",
-                    "nombre": "Europac Madrid Dueñas",
-                    "latitud": "40.47799923859763",
-                    "longitud": "-3.7035075500000403",
-                    "direcciones": [
-                        {
-                            "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
-                        },
-                        {
-                            "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
-                        }
-                    ]
-                },
-                {
-                    "lugar": "Barcelona",
-                    "nombre": "Europac Dueñas",
-                    "latitud": "41.392700620871764",
-                    "longitud": "2.139299449999953",
-                    "direcciones": [
-                        {
-                            "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
-                        },
-                        {
-                            "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
-                        }
-                    ]
-                },
-                {
-                    "lugar": "Ibiza",
-                    "nombre": "Europac Dueñas",
-                    "latitud": "38.910339674213816",
-                    "longitud": "1.423660400000017",
-                    "direcciones": [
-                        {
-                            "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
-                        },
-                        {
-                            "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "pais": "Francia",
-            "lugares": [
-                {
-                    "lugar": "Coruña",
-                    "nombre": "Europac Dueñas",
-                    "latitud": "43.34567700262167",
-                    "longitud": "-8.422813299999916",
-                    "direcciones": [
-                        {
-                            "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
-                        },
-                        {
-                            "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
-                        }
-                    ]
-                },
-                {
-                    "lugar": "Pamplona",
-                    "nombre": "Europac Dueñas",
-                    "latitud": "42.81574252263008",
-                    "longitud": "-1.6496008000000302",
-                    "direcciones": [
-                        {
-                            "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
-                        },
-                        {
-                            "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
-                        }
-                    ]
-                },
-                {
-                    "lugar": "Burgos",
-                    "nombre": "Europac Dueñas",
-                    "latitud": "42.344421110653975",
-                    "longitud": "-3.689439450000009",
-                    "direcciones": [
-                        {
-                            "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
-                        },
-                        {
-                            "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "pais": "Portugal",
-            "lugares": [
-                {
-                    "lugar": "Madrid",
-                    "nombre": "Europac Dueñas",
-                    "latitud": "40.47799923859763",
-                    "longitud": "-3.7035075500000403",
-                    "direcciones": [
-                        {
-                            "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
-                        },
-                        {
-                            "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
-                        }
-                    ]
-                },
-                {
-                    "lugar": "Barcelona",
-                    "nombre": "Europac Dueñas",
-                    "latitud": "41.392700620871764",
-                    "longitud": "2.139299449999953",
-                    "direcciones": [
-                        {
-                            "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
-                        },
-                        {
-                            "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
-                        }
-                    ]
-                },
-                {
-                    "lugar": "Ibiza",
-                    "nombre": "Europac Dueñas",
-                    "latitud": "38.910339674213816",
-                    "longitud": "1.423660400000017",
-                    "direcciones": [
-                        {
-                            "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
-                        },
-                        {
-                            "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
+    "paises": [{
+        "pais": "España",
+        "lugares": [{
+            "lugar": "Madrid",
+            "nombre": "Europac Madrid Dueñas",
+            "latitud": "40.47799923859763",
+            "longitud": "-3.7035075500000403",
+            "direcciones": [{
+                "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
+            }, {
+                "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
+            }]
+        }, {
+            "lugar": "Barcelona",
+            "nombre": "Europac Dueñas",
+            "latitud": "41.392700620871764",
+            "longitud": "2.139299449999953",
+            "direcciones": [{
+                "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
+            }, {
+                "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
+            }]
+        }, {
+            "lugar": "Ibiza",
+            "nombre": "Europac Dueñas",
+            "latitud": "38.910339674213816",
+            "longitud": "1.423660400000017",
+            "direcciones": [{
+                "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
+            }, {
+                "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
+            }]
+        }]
+    }, {
+        "pais": "Francia",
+        "lugares": [{
+            "lugar": "Coruña",
+            "nombre": "Europac Dueñas",
+            "latitud": "43.34567700262167",
+            "longitud": "-8.422813299999916",
+            "direcciones": [{
+                "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
+            }, {
+                "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
+            }]
+        }, {
+            "lugar": "Pamplona",
+            "nombre": "Europac Dueñas",
+            "latitud": "42.81574252263008",
+            "longitud": "-1.6496008000000302",
+            "direcciones": [{
+                "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
+            }, {
+                "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
+            }]
+        }, {
+            "lugar": "Burgos",
+            "nombre": "Europac Dueñas",
+            "latitud": "42.344421110653975",
+            "longitud": "-3.689439450000009",
+            "direcciones": [{
+                "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
+            }, {
+                "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
+            }]
+        }]
+    }, {
+        "pais": "Portugal",
+        "lugares": [{
+            "lugar": "Madrid",
+            "nombre": "Europac Dueñas",
+            "latitud": "40.47799923859763",
+            "longitud": "-3.7035075500000403",
+            "direcciones": [{
+                "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
+            }, {
+                "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
+            }]
+        }, {
+            "lugar": "Barcelona",
+            "nombre": "Europac Dueñas",
+            "latitud": "41.392700620871764",
+            "longitud": "2.139299449999953",
+            "direcciones": [{
+                "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
+            }, {
+                "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
+            }]
+        }, {
+            "lugar": "Ibiza",
+            "nombre": "Europac Dueñas",
+            "latitud": "38.910339674213816",
+            "longitud": "1.423660400000017",
+            "direcciones": [{
+                "direccion": "Ctra. Burgos-Portugal Km. 96, 34210 Dueñas (Palencia)"
+            }, {
+                "direccion": "+34 979 761 413 (Fax: +34 979 761 570)"
+            }]
+        }]
+    }]
 }
